cd7530cd48180fcf4953b0a47172c92c56db9f59
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-06-14 13:40:55 +0530

    add og headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-
 function App() {
   return (
-    <div className="bg-[url('./assets/image_1.png')] h-screen bg-contain bg-no-repeat bg-center">
-      <h1 className="text-2xl md:text-4xl p-2 font-bold text-center text-white from-red-500 to-yellow-500 bg-gradient-to-t">Prasad Akula</h1>
+    <div className="bg-black bg-[url('./assets/image_1.png')] h-screen bg-contain bg-no-repeat bg-center flex flex-col">
+      <h1 className="text-2xl md:text-4xl p-2 font-bold text-center text-white from-red-500 to-yellow-500 bg-gradient-to-t">
+        Prasad Akula 🎆🎆🎆🎆
+      </h1>
+
+      <div className="mt-auto">
+        <p className="leading-3 text-3xl md:text-6xl text-center font-bold text-white bg-black p-3 ">
+          Wishing you very happy birthday 🎂🎆🎆
+        </p>
+        <p className="leading-3 text-3xl drop-shadow-lg border border-t-1 border-b-0 border-l-0 border-r-0 border-t-white md:text-6xl text-center font-bold text-white bg-black p-3 ">
+          God bless you abundantly 🎉
+        </p>
+      </div>
     </div>
   );
 }
